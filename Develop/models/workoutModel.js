84c5301920b8b844812667c570c3548e7ca254fa
tdj03001 <tyjenki@gmail.com>
2020-03-28 18:26:21 -0400

    finalized schema

--- Develop/models/workoutModel.js
+++ Develop/models/workoutModel.js
@@ -3,21 +3,67 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for workout"
-  },
-  value: {
-    type: Number,
-    required: "Enter a workout duration"
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+
+
+  day: { type: Date, default: () => new Date() },
+  exercises: [{
+    type: {
+      type: String,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter excercise name:"
+    },
+    duration: {
+      type: Number,
+      trim: true,
+      required: "Enter excercise duration:",
+      validate: {
+        validator: Number.isInteger,
+        message: '{VALUE} is not an integer value'
+      }
+    },
+    weight: {
+      type: Number,
+      trim: true,
+      required: "Enter weight amount:",
+      validate: {
+        validator: Number.isInteger,
+        message: '{VALUE} is not an integer value'
+      }
+    },
+    reps: {
+      type: Number,
+      trim: true,
+      required: "Enter number of reps:",
+      validate: {
+        validator: Number.isInteger,
+        message: '{VALUE} is not an integer value'
+      }
+    },
+    sets: {
+      type: Number,
+      trim: true,
+      required: "Enter number of sets:",
+      validate: {
+        validator: Number.isInteger,
+        message: '{VALUE} is not an integer value'
+      }
+    },
+    distance: {
+      type: Number,
+      trim: true,
+      required: "Enter number of reps:",
+      validate: {
+        validator: Number.isInteger,
+        message: '{VALUE} is not an integer value'
+      }
+    }
+  }]
+
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
-module.exports = Workout;
+module.exports = { "Workout": Workout }
