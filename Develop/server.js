b29f617f2b3f5054d801e756c19d5fcbc67cc568
tdj03001 <tyjenki@gmail.com>
2020-03-28 18:32:20 -0400

    added app.use for both the api and the html routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -10,13 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 
 // app.use(require("./public/api"));
+app.use(require("./routes/apiRoutes.js"))
+app.use(require("./routes/htmlRoutes.js"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
