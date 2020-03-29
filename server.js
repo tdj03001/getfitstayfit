24696ef63c28967d6e6651586aa53382b25adb1a
tdj03001 <tyjenki@gmail.com>
2020-03-29 11:56:58 -0400

    added mlab URL back in after successful localhost test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://tdj03001:101%Sure!!@ds213529.mlab.com:13529/heroku_f83j55v9", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
