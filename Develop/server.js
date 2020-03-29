e788332086bceaa96ff30825f341144323449b32
tdj03001 <tyjenki@gmail.com>
2020-03-28 20:43:19 -0400

    heroku

--- Develop/server.js
+++ Develop/server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+  // useNewUrlParser: true,
+  // useFindAndModify: false
 });
 
 
