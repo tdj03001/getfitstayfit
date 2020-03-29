0627ee23bfbf1feae7ba032dfae2d388faf001e1
tdj03001 <tyjenki@gmail.com>
2020-03-28 20:13:51 -0400

    heroku troubles

--- Develop/server.js
+++ Develop/server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://tdj03001:101%Sure!!@ds213529.mlab.com:13529/heroku_8h9bcsw8", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
